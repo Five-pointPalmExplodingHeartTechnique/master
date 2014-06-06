fa76b49a03b952f028051110858675bd76bf8d71
Matthew Ramos <RevZero@Everetts-MacBook-Pro.local>
2014-06-06 16:59:13 -0700

    setIndexBlock

--- Inode.java
+++ Inode.java
@@ -1,8 +1,11 @@
 
 public class Inode {
 
-    private final static int iNodeSize = 32;       // fix to 32 bytes
+    private final static int iNodeSize  = 32;      // fix to 32 bytes
     private final static int directSize = 11;      // # direct pointers
+
+    private final int shortbyte = 2;
+    private final int intbyte = 2;
 
     public int length;                             // file size in bytes
     public short count;                            // # file-table entries pointing to this
@@ -17,27 +20,43 @@ public class Inode {
         count = 0;
         flag = 1;
 
-        for ( int i = 0; i < directSize; i++ ) {
+        for (int i = 0; i < directSize; i++) {
             direct[i] = -1;
         }
 
         indirect = -1;
     }
+
 // -----------------------------------------------------------------------------
-    Inode( short iNumber ) {                       // retrieving inode from disk
+// Constructor
+// Retrieving inode from disk
+    Inode( short iNumber ) {
         
         int blockNumber = 1 + iNumber / 16
         byte[] data = new byte[Disk.blockSize];
         SysLib.rawread( blockNumber, data );
         int offset = ( iNumber % 16 ) * 32;
 
+        // Length
         length = SysLib.bytes2int( data, offset );
         offset += 4;
 
+        // Count
         count = SysLib.bytes2short( data, offset );
         offset += 2;
 
+        // Flag
         flag = SysLib.bytes2short( data, offset );
+        offset += 2;
+
+        // Direct
+        for (int i = 0; i < directSize; i++) {
+            direct[i] = SysLib.bytes2short( data, offset);
+            offset += 2;
+        }
+
+        // Indirect
+        indirect = SysLib.bytes2short( data, offset);
         offset += 2;
 
     }
@@ -49,7 +68,6 @@ public class Inode {
 
         byte[] iNode = new byte[iNodeSize];
         int offset = 0;
-
 
         // Length - int2bytes( int i, byte[] b, int offset )
         SysLib.int2bytes( length, iNode,  offset );
@@ -73,7 +91,6 @@ public class Inode {
         SysLib.short2bytes( indirect, iNode, offset );  
         offset += 2;
 
-
         int blockNumber = 1 + iNumber / 16
         byte[] data = new byte[Disk.blockSize];
         SysLib.rawread( blockNumber, data );
@@ -81,8 +98,6 @@ public class Inode {
 
         System.arraycopy( iNode, 0, data, offset, iNodeSize );
         SysLib.rawwrite( blockNumber, data );
-
-
     }
 
 // -----------------------------------------------------------------------------
@@ -92,6 +107,16 @@ public class Inode {
 
 // -----------------------------------------------------------------------------
     public boolean setIndexBlock( short getIndexBlockNumber ) {
+
+        for ( int i = 0; i < directSize; i++ ) {
+            // Checks direct if it is -1
+            if ( direct[i] == -1 ) {
+                return false;
+            }
+        }
+
+        // Check indirect
+
         return true;
     }
 
