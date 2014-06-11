5e68ccf7f98891d7721a79c7e9ed4ed3a01ccb7d
fatbobby <fatbobby@uw.edu>
2014-06-10 22:33:20 -0700

    Update TCB.java

--- TCB.java
+++ TCB.java
@@ -1,10 +1,18 @@
+/**
+ * class TCB
+ * @author Ryan Aarestad
+ */
+
 public class TCB {
+    private final int MAX_ENTRIES = 32;
+    private final int RESERVED_ENTRIES = 3;
     private Thread thread = null;
     private int tid = 0;
     private int pid = 0;
     private boolean terminated = false;
     private int sleepTime = 0;
     public FileTableEntry[] ftEnt = null; // added for the file system
+    private int fileTableSize = RESERVED_ENTRIES;
 
     public TCB( Thread newThread, int myTid, int parentTid ) {
 		thread = newThread;
@@ -12,7 +20,7 @@ public class TCB {
 		pid = parentTid;
 		terminated = false;
 
-		ftEnt = new FileTableEntry[32];    // added for the file system
+		ftEnt = new FileTableEntry[MAX_ENTRIES];    // added for the file system
 
 		System.err.println( "threadOS: a new thread (thread=" + thread + 
 				    " tid=" + tid + 
@@ -44,7 +52,7 @@ public class TCB {
     public synchronized int getFd( FileTableEntry entry ) {
 		if ( entry == null )
 		    return -1;
-		for ( int i = 3; i < 32; i++ ) {
+		for ( int i = RESERVED_ENTRIES; i < MAX_ENTRIES; i++ ) {
 		    if ( ftEnt[i] == null ) {
 			ftEnt[i] = entry;
 			return i;
@@ -54,21 +62,76 @@ public class TCB {
     }
 
     // added for the file system
+    // removes from the FIleTable and returns an FDEntry at the param index
     public synchronized FileTableEntry returnFd( int fd ) {
-		if ( fd >= 3 && fd < 32 ) {
+		if ( fd >= RESERVED_ENTRIES && fd < MAX_ENTRIES ) {
 		    FileTableEntry oldEnt = ftEnt[fd];
 		    ftEnt[fd] = null;
-		    return oldEnt;
-		}
-		else
+
+            // if this creates a hole, close it and adjust size with collapse()
+            if( ( fd < (MAX_ENTRIES - 1) ) && ( ftEnt[fd + 1] != null ) )
+                collapse(fd);
+
+            return oldEnt;
+        }
+		//else
 		    return null;
     }
 
     // added for the file systme
     public synchronized FileTableEntry getFtEnt( int fd ) {
-		if ( fd >= 3 && fd < 32 )
+		if ( fd >= RESERVED_ENTRIES && fd < MAX_ENTRIES )
 		    return ftEnt[fd];
 		else
 		    return null;
+    }
+
+    /**
+    * adds new entries to the back side of ftEnt[], exhibiting FIFOa new entry
+    * TODO: make sure any 'back' commands take from the front end
+    * @return the value of the index in ftEnt into which 'entry' was placed,
+    * or -1 on a failure.  makes no distinction as to whether or not FIFO
+    * recycling was performed
+    * when the table is full, this method discards the oldest half of the
+    * history, which facilitates simple writes rather than O(n) traversals
+    * and writes with each call once full
+    */
+    public synchronized int addFTEnt(FileTableEntry entry) {
+
+        // if the table is not full, there's no need to rewrite
+        // the table
+        if (!this.isTableFull()) {
+            ftEnt[fileTableSize++] = entry;
+            return fileTableSize;
+        }
+
+        // else we've gotta move table entries to make room for the noob
+        // cut the back half of the history
+        else {
+            // cut the history in half
+            fileTableSize /= 2;
+            // now set the oldest half = newest half, replacing the old
+            for (int i = RESERVED_ENTRIES; i < fileTableSize; i++)
+                ftEnt[i] = ftEnt[i + fileTableSize];
+
+            ftEnt[fileTableSize++] = entry;
+            return fileTableSize;
+        }
+     } // end method addFTEnt
+
+    private boolean isTableFull(){
+        // yes, I know size should never go over the max, but slight
+        // redundancy FTW
+        return (fileTableSize >= MAX_ENTRIES);
+    }
+
+    // this method closes up a hole caused by returnFd and adjusts the size of
+    // the history accordingly
+    private void collapse(int index){
+        for (int i = index; i < fileTableSize; i++)
+            ftEnt[i] = ftEnt[i + 1];
+
+        fileTableSize--;
+        //return true;
     }
 }
