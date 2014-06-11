78aa720fe59d9da23d6d642756ebe544a9be3ea3
Dat Nguyen <dat.nguyen92@hotmail.com>
2014-06-11 13:21:16 -0700

    Implement deallocateBlocks

--- FileSystem.java
+++ FileSystem.java
@@ -40,6 +40,45 @@ public class FileSystem {
     int write( FileTableEntry ftEnt, byte[] buffer ) {
     }
     private boolean deallocAllBlocks( FileTableEntry ftEnt ) {
+        // Retrieve inode from file table entry. If inode is null, return false.
+        Inode inode = ftEnt.inode;
+        if(inode == null) return false;
+
+        // Deallocate all 11 direct blocks in inode. Iterate 11 blocks until
+        // reach the end of direct blocks ( hit -1 ).
+        for(int i = 0; i < inode.direct.length; i++){
+            // Block to be deallocate
+            int blockID = inode.direct[i];
+            // If hit the last block, get out of loop
+            if(blockID == -1) break;
+            // Return the block back to superblock
+            superblock.returnBlock(blockID);
+            // Set the block to -1
+            inode.direct[i] = -1;
+        }
+
+        // Deallocate indirect block in inode.
+        if(inode.indirect != -1){
+            // Load the current indirect blocks
+            byte[] b = new byte[Disk.blockSize];
+            SysLib.rawread(inode.indirect, b);
+            // Iterate through the indirect block until reach the end (hit -1)
+            for(int i = 0; i < b.length; i += 2){
+                // Get the indirect pointer
+                int blockID = SysLib.bytes2short(b,i);
+                // If hit the last indirect pointer, get out of loop
+                if(blockID == -1) break;
+                // Return the block back to superblock
+                superblock.returnBlock(blockID);
+                // Set the block to -1
+                SysLib.short2bytes((short) -1, b, i);
+            }
+            // Write back indirect block
+            SysLib.rawwrite(inode.indirect, b);
+        }
+        // Reset seek pointer to beginning of file
+        ftEnt.seekPtr = 0;
+        return true;
     }
     // Delete a file. Release the corresponding file table entry from directory
     boolean delete( String filename ) {
